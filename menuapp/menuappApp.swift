e9afa4e0126f241b495db632cf23fde66dba4d58
pegahtafviziingoampt <ingoampt@gmail.com>
2023-07-18 12:05:44 +0200

    Initial Commit

---
+++ menuapp/menuappApp.swift
@@ -0,0 +1,17 @@
+//
+//  menuappApp.swift
+//  menuapp
+//
+//  Created by Pegah Tafvizi on 18.07.23.
+//
+
+import SwiftUI
+
+@main
+struct menuappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
